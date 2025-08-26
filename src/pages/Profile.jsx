ba61d2d3d504546d1984ad9623c281f001bd85e5
LautaroLeall <lautaroleal4@gmail.com>
2025-08-26 12:47:05 -0300

    new componets (Profile)

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,21 @@
+// src/pages/Profile.jsx
+import React from 'react'
+import { useAuth } from "../hooks/useAuth";
+import NavBar from "../components/ui/Navbar";
+
+const Profile = () => {
+    const { getCurrentUser } = useAuth();
+    const user = getCurrentUser();
+
+    return (
+        <>
+            <NavBar />
+            <div className="container mt-5">
+                <h2>Mi Perfil</h2>
+                <p>Bienvenido, <strong>{user?.email}</strong></p>
+            </div>
+        </>
+    )
+}
+
+export default Profile
