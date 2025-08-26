67678105d5895128e10720cb44324414f14a8f74
LautaroLeall <lautaroleal4@gmail.com>
2025-08-25 22:34:31 -0300

    config for routes (routes and private routes)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from "./context/AuthProvider";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,8 +9,10 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
