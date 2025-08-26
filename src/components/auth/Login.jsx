dfa06ec2d267af4d8a41269fce8c6a4cbd8cfbcc
LautaroLeall <lautaroleal4@gmail.com>
2025-08-26 12:31:38 -0300

    change in login

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -7,9 +7,10 @@
 
 import { useState } from "react";
 import { useAuth } from "../../hooks/useAuth";
-import { useNavigate } from "react-router-dom";
-import { IoEyeSharp } from "react-icons/io5";  // Icono ver contraseña
-import { FaEyeSlash } from "react-icons/fa";   // Icono ocultar contraseña
+import { Link, useNavigate } from "react-router-dom";
+import { IoEyeSharp } from "react-icons/io5";
+import { FaEyeSlash } from "react-icons/fa"; 
+import { RiExpandLeftLine } from "react-icons/ri";
 import "../../styles/Login.css";
 
 export default function Login() {
@@ -38,48 +39,59 @@ export default function Login() {
     };
 
     return (
-        <div className="login-container">
-            <h2 className="login-title">Iniciar sesión</h2>
-            <p className="login-subtitle">Usa tu email o tu nombre de usuario.</p>
+        <div className="container">
+            <Link to="/" className="back-link-login">
+                <RiExpandLeftLine />
+            </Link>
+            <div className="login-container">
+                <h2 className="login-title">Iniciar sesión</h2>
+                <p className="login-subtitle">Usa tu email o tu nombre de usuario.</p>
 
-            <form className="login-form" onSubmit={handleSubmit}>
-                {error && <div className="alert alert-danger">{error}</div>}
+                <form className="login-form" onSubmit={handleSubmit}>
+                    {error && <div className="alert alert-danger">{error}</div>}
 
-                <div className="form-group mb-3">
-                    <input
-                        name="identifier"
-                        value={form.identifier}
-                        onChange={handleChange}
-                        className="form-control"
-                        placeholder="tu@correo.com o tu_usuario"
-                        required
-                    />
-                </div>
-
-                <div className="form-group mb-3">
-                    <div className="input-group d-flex align-items-center">
+                    <div className="form-group mb-3">
                         <input
-                            name="password"
-                            value={form.password}
+                            name="identifier"
+                            value={form.identifier}
                             onChange={handleChange}
-                            type={showPasswords ? "text" : "password"}
-                            placeholder="contraseña"
                             className="form-control"
+                            placeholder="tu@correo.com o tu_usuario"
                             required
                         />
-                        <span
-                            className="password-toggle ms-2"
-                            onClick={() => setShowPasswords(s => !s)}
-                        >
-                            {showPasswords ? <FaEyeSlash /> : <IoEyeSharp />}
-                        </span>
                     </div>
-                </div>
 
-                <div className="d-flex justify-content-center">
-                    <button className="btn-login">Entrar</button>
-                </div>
-            </form>
+                    <div className="form-group mb-3">
+                        <div className="input-group d-flex align-items-center">
+                            <input
+                                name="password"
+                                value={form.password}
+                                onChange={handleChange}
+                                type={showPasswords ? "text" : "password"}
+                                placeholder="contraseña"
+                                className="form-control"
+                                required
+                            />
+                            <span
+                                className="password-toggle ms-2"
+                                onClick={() => setShowPasswords(s => !s)}
+                            >
+                                {showPasswords ? <FaEyeSlash /> : <IoEyeSharp />}
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="d-flex justify-content-center mb-3">
+                        <button className="btn-login">Entrar</button>
+                    </div>
+
+                    <div className="d-flex justify-content-end">
+                        <Link to="/register" className="btn-link">
+                            ¿No tienes cuenta?
+                        </Link>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
